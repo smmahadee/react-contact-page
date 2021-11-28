ce2f87f06b38dbca46a0f88cd89627f453d99498
smmahadee <smmahadee0@gmail.com>
2021-11-28 14:41:16 +0600

    no contact found div created

--- src/Component/ShowContact.js
+++ src/Component/ShowContact.js
@@ -15,29 +15,37 @@ export default function ShowContact({ filterValue }) {
   });
 
   // created all contacts element
-  const contactData = contacts.map((el, i) => {
-    const { isLetter, firstName, lastName, phone, imgSrc } = el;
-    if (isLetter) {
+
+  let contactData;
+
+  if (contacts.length !== 0) {
+    contactData = contacts.map((el, i) => {
+      const { isLetter, firstName, lastName, phone, imgSrc } = el;
+
+      if (isLetter) {
+        return (
+          <div key={i} className='text-center text-xs'>
+            {firstName}
+          </div>
+        );
+      }
+
       return (
-        <div key={i} className='text-center text-xs'>
-          {firstName}
+        <div
+          key={i}
+          className='mx-3 my-1 bg-white rounded px-2 flex space-x-3 items-center py-1'
+        >
+          <img className='rounded-full h-8' src={imgSrc} alt='' />
+          <div>
+            <h4 className='text-black'>{firstName + ' ' + lastName}</h4>
+            <p className='text-xs'>{phone}</p>
+          </div>
         </div>
       );
-    }
-
-    return (
-      <div
-        key={i}
-        className='mx-3 my-1 bg-white rounded px-2 flex space-x-3 items-center py-1'
-      >
-        <img className='rounded-full h-8' src={imgSrc} alt='' />
-        <div>
-          <h4 className='text-black'>{firstName + ' ' + lastName}</h4>
-          <p className='text-xs'>{phone}</p>
-        </div>
-      </div>
-    );
-  });
+    });
+  } else {
+    contactData = <div className='text-center mt-3'>No Contact Found</div>;
+  }
 
   return contactData;
 }
