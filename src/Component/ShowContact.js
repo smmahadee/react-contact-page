c22c8e676d5f3a49e20dde0eb3750b2f2d245ada
smmahadee <smmahadee0@gmail.com>
2021-11-28 12:28:41 +0600

    some changes done

--- src/Component/ShowContact.js
+++ src/Component/ShowContact.js
@@ -16,7 +16,7 @@ export default function ShowContact({ filterValue }) {
 
   // created all contacts element
   const contactData = contacts.map((el, i) => {
-    const { isLetter, firstName, lastName, phone } = el;
+    const { isLetter, firstName, lastName, phone, imgSrc } = el;
     if (isLetter) {
       return (
         <div key={i} className='text-center text-xs'>
@@ -24,13 +24,13 @@ export default function ShowContact({ filterValue }) {
         </div>
       );
     }
-    
+
     return (
       <div
         key={i}
         className='mx-3 my-1 bg-white rounded px-2 flex space-x-3 items-center py-1'
       >
-        <img className='rounded-full h-8' src={el.imgSrc} alt='' />
+        <img className='rounded-full h-8' src={imgSrc} alt='' />
         <div>
           <h4 className='text-black'>{firstName + ' ' + lastName}</h4>
           <p className='text-xs'>{phone}</p>
